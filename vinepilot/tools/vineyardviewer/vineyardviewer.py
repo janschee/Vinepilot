250db16988e9c43c354593673302533aa640f130
janschee <jan.scheerer@icloud.com>
2023-11-28 20:29:49 +0000

    add css and started frame control

--- vinepilot/tools/vineyardviewer/vineyardviewer.py
+++ vinepilot/tools/vineyardviewer/vineyardviewer.py
@@ -13,7 +13,8 @@ class VineyardViewer():
         self.port: int = Project.port
         self.host_address: str = Project.host_address
         self.templates: str = os.path.normpath(os.path.join(Project.base_dir, "./vinepilot/tools/vineyardviewer/templates")) 
-        self.app = Flask(__name__, template_folder = self.templates)
+        self.static: str = os.path.normpath(os.path.join(Project.base_dir, "./vinepilot/tools/vineyardviewer/static")) 
+        self.app = Flask(__name__, template_folder = self.templates, static_folder = self.static)
 
         #Routes
         self.app.route("/")(self.home)
@@ -26,6 +27,7 @@ class VineyardViewer():
         self.position: tuple = (0,0)
         self.rotation: float = 0.0
         self.zoom_factor: float = 1.0
+        self.frame: int = 0
 
         #Images
         self.virtual = None
@@ -49,6 +51,11 @@ class VineyardViewer():
         img.save(self.virtual, "PNG")
         self.virtual.seek(0)
         return send_file(self.virtual, mimetype="image/png", as_attachment=True, download_name="virtual.png")
+    
+    def switch_frame(self, frame_number = None, step_size = None):
+        assert not bool((frame_number is not None) and (step_size is not None)), "Input either frame_number or step_size, not both!"
+        if frame_number is not None: self.frame = int(frame_number)
+        if step_size is not None: self.frame += int(step_size)
         
 
     #Pages
@@ -58,8 +65,9 @@ class VineyardViewer():
                             current_pos_y = self.position[0],
                             current_pos_x = self.position[1],
                             current_rotation = self.rotation,
+                            current_zoom_factor = self.zoom_factor,
+                            current_frame = self.frame,
                             virtual = url_for("load_virtual"))
-
     #Actions
     def button_plus100(self):
         self.vineyard_number += 100
@@ -68,7 +76,11 @@ class VineyardViewer():
     def submit_parameters(self):
         self.position = (float(request.form.get("pos_y")), float(request.form.get("pos_x")))
         self.rotation = float(request.form.get("rotation"))
+        self.zoom_factor = float(request.form.get("zoom_factor"))
         return self.home()
+    
+    def button_next_frame(self):
+        pass
 
 
     def show(self):
