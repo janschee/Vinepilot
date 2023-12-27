2e376cce9db1e361b02e225e8124ecc68ce14f9c
janschee <jan.scheerer@icloud.com>
2023-12-27 18:28:17 +0000

    transposed convolution

--- vinepilot/model/model.py
+++ vinepilot/model/model.py
@@ -8,8 +8,8 @@ class PrintLayer(torch.nn.Module):
     def forward(self, x):
         print(x.shape)
         return x
-    
 
+""" 
 class UpSample(torch.nn.Module):
     def __init__(self, scale_factor):
         super(UpSample, self).__init__()
@@ -17,34 +17,45 @@ class UpSample(torch.nn.Module):
 
     def forward(self, x):
         return torch.nn.functional.interpolate(x, scale_factor=self.scale_factor, mode="nearest")
+""" 
     
+class Reshape(torch.nn.Module):
+    def __init__(self, shape):
+        super().__init__()
+        self.shape = shape
+
+    def forward(self, x):
+        return torch.reshape(x, self.shape)
 
 class SegmantationModel(torch.nn.Module):
     def __init__(self):
-        """
-        self.convDOWN = torch.nn.Conv2d(1, 1, kernel_size=5, stride=1, padding=0)        
-        self.convUP = torch.nn.Conv2d(1, 1, kernel_size=5, stride=1, padding=4)
-        self.convCONST = torch.nn.Conv2d(1, 1, kernel_size=5, stride=1, padding=2)
-        self.relu = torch.nn.ReLU()  
-        """
         super(SegmantationModel, self).__init__()
+
         self.encoder = torch.nn.Sequential(
-            torch.nn.Conv2d(1, 8, kernel_size=(3, 3), stride=2, padding=1),
+            torch.nn.Conv2d(1, 32, kernel_size=(3, 3), stride=2, padding=1),
             torch.nn.ReLU(),
-            torch.nn.Conv2d(8, 16, kernel_size=(3, 3), stride=2, padding=1),
+            torch.nn.Conv2d(32, 16, kernel_size=(3, 3), stride=2, padding=1),
             torch.nn.ReLU(),
-            torch.nn.Conv2d(16, 32, kernel_size=(3, 3), stride=2, padding=1),
+            torch.nn.Conv2d(16, 8, kernel_size=(3, 3), stride=2, padding=1),
             torch.nn.ReLU(),
         )
+
+        self.fully_connected = torch.nn.Sequential(
+            torch.nn.Flatten(),
+            torch.nn.Linear(8 * 16 * 32, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, 512),
+            torch.nn.ReLU(),
+            Reshape((5, 1, 16, 32)) #TODO: Dont use fixed shapes!
+        )
+
         self.decoder = torch.nn.Sequential(
-            UpSample(scale_factor=2),
-            torch.nn.Conv2d(32, 16, kernel_size=(3, 3), stride=1, padding=1),
+            torch.nn.ConvTranspose2d(1, 1, kernel_size=(3, 3), stride=2, padding=1),
+            PrintLayer(),
             torch.nn.ReLU(),
-            UpSample(scale_factor=2),
-            torch.nn.Conv2d(16, 8, kernel_size=(3, 3), stride=1, padding=1),
+            torch.nn.ConvTranspose2d(1, 1, kernel_size=(3, 3), stride=2, padding=1),
             torch.nn.ReLU(),
-            UpSample(scale_factor=2),
-            torch.nn.Conv2d(8, 1, kernel_size=(3, 3), stride=1, padding=1),
+            torch.nn.ConvTranspose2d(1, 1, kernel_size=(3, 3), stride=2, padding=1),
             torch.nn.ReLU(),
         )
 
@@ -55,6 +66,7 @@ class SegmantationModel(torch.nn.Module):
         x = grayscale(input)
 
         x = self.encoder(x)
+        x = self.fully_connected(x)
         x = self.decoder(x) 
 
         return x
